52465bf1bc959b4a607f39f017d24357dfdc4ccb
Petr Filaretov <petr.filaretov@gmail.com>
2021-11-23 10:53:40 +0300

    initial commit

---
+++ src/test/java/pro/filaretov/spring/data/equalshashcode/SpringDataEqualsHashcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.data.equalshashcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataEqualsHashcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
