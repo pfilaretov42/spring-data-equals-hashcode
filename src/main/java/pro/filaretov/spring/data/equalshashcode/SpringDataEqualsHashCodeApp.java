1b42a30ee49a7512871dfd1a3eefa8947afb8339
Petr Filaretov <petr.filaretov@gmail.com>
2021-11-23 11:18:38 +0300

    refactoring

--- src/main/java/pro/filaretov/spring/data/equalshashcode/SpringDataEqualsHashCodeApp.java
+++ src/main/java/pro/filaretov/spring/data/equalshashcode/SpringDataEqualsHashCodeApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataEqualsHashcodeApplication {
+public class SpringDataEqualsHashCodeApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataEqualsHashcodeApplication.class, args);
+        SpringApplication.run(SpringDataEqualsHashCodeApp.class, args);
     }
 
 }
